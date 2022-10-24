e813f421b2d2cc007d188d6c6948a31e4910e8e4
Allen Pocket <AllenPocketGamer@Gmail.com>
2022-10-24 22:39:15 +0800

    :recycle: ($FFI) refactor agent

--- ic-agent-ffi/src/agent/mod.rs
+++ ic-agent-ffi/src/agent/mod.rs
@@ -1,28 +1,42 @@
 use crate::identity::IdentityType;
 use crate::{ret_thin_ptr, ret_unsized, AnyErr, StateCode, UnsizedCallBack};
 use anyhow::anyhow;
-use ic_agent::agent::http_transport::ReqwestHttpReplicaV2Transport;
 use ic_agent::identity::{AnonymousIdentity, Identity, Secp256k1Identity};
-use ic_agent::Agent;
-use libc::c_char;
+use ic_types::Principal;
+use libc::{c_char, c_int};
 use std::ffi::CStr;
+
+pub struct AgentWrapper {
+    url: String,
+    identity: Box<dyn Identity>,
+    canister_id: Principal,
+    did_content: String,
+}
 
 /// NOTE: Forbid inputting [`BasicIdentity`] because:
 ///
-/// 1. `p2fptr` will be dangling because the creation of [`Agent`] will consume it.
-/// 2. `[BasicIdentity]` doesn't implement [`Clone`] trait because it's inner field.
+/// 1. `[BasicIdentity]` doesn't implement [`Clone`] trait because it's inner field.
 #[no_mangle]
 pub extern "C" fn agent_create(
+    // Url points to the ic net
     url: *const c_char,
+    // The pointer points to the [`Identity`]
     p2fptr_iden: *const *const dyn Identity,
+    // The type of [`Identity`]
     iden_type: IdentityType,
-    p2ptr_agent: *mut *const Agent,
+    // The data of [`Principal`]
+    canister_id_bytes: *const u8,
+    // The length of data of [`Principal`]
+    canister_id_bytes_len: c_int,
+    // The content of candid of that canister
+    did_content: *const c_char,
+    // out: A pointer points to the `AgentWrapper`
+    p2ptr_agent_wrapped: *mut *const AgentWrapper,
+    // The callback used report error information
     err_cb: UnsizedCallBack,
 ) -> StateCode {
-    let once = || -> Result<Agent, AnyErr> {
-        let url = unsafe { CStr::from_ptr(url).to_str().map_err(AnyErr::from) }?;
-
-        let transport = ReqwestHttpReplicaV2Transport::create(url).map_err(AnyErr::from)?;
+    let once = || -> Result<AgentWrapper, AnyErr> {
+        let url = unsafe { CStr::from_ptr(url).to_str().map_err(AnyErr::from) }?.to_string();
 
         let identity = unsafe {
             match iden_type {
@@ -44,19 +58,26 @@ pub extern "C" fn agent_create(
             }
         };
 
-        let agent = Agent::builder()
-            .with_transport(transport)
-            .with_boxed_identity(identity)
-            .build()
-            .map_err(AnyErr::from)?;
+        let slice = unsafe {
+            std::slice::from_raw_parts(canister_id_bytes, canister_id_bytes_len as usize)
+        };
+        let canister_id = Principal::from_slice(slice);
 
-        Ok(agent)
+        let did_content =
+            unsafe { CStr::from_ptr(did_content).to_str().map_err(AnyErr::from) }?.to_string();
+
+        Ok(AgentWrapper {
+            url,
+            identity,
+            canister_id,
+            did_content,
+        })
     };
 
     match once() {
-        Ok(agent) => {
+        Ok(agent_w) => {
             unsafe {
-                ret_thin_ptr(p2ptr_agent, agent);
+                ret_thin_ptr(p2ptr_agent_wrapped, agent_w);
             }
 
             StateCode::Ok
@@ -69,31 +90,55 @@ pub extern "C" fn agent_create(
     }
 }
 
+#[no_mangle]
+pub extern "C" fn agent_query() -> StateCode {
+    todo!()
+}
+
+#[no_mangle]
+pub extern "C" fn agent_update() -> StateCode {
+    todo!()
+}
+
+#[no_mangle]
+pub extern "C" fn agent_status() -> StateCode {
+    todo!()
+}
+
 #[cfg(test)]
 mod tests {
-    use ic_agent::identity::BasicIdentity;
     use super::*;
-    use crate::identity::{identity_anonymous, identity_secp256k1_random};
+    use crate::identity::{identity_anonymous, identity_basic_random, identity_secp256k1_random};
     use crate::tests_util::{apply_fptr, apply_ptr};
+    use ic_agent::identity::BasicIdentity;
     use ic_types::Principal;
     use libc::c_int;
 
-    const MAIN_NET: &[u8; 16] = b"https://ic0.app\0";
+    const IC_NET_BYTES: &[u8; 16] = b"https://ic0.app\0";
+    const CANISTER_ID_BYTES: &[u8; 1] = &[4u8];
+    const DID_CONTENT_BYTES: &[u8; 5] = b"todo\0";
+
+    fn cbytes_to_str(cbytes: &[u8]) -> &str {
+        let cstr = CStr::from_bytes_with_nul(cbytes).unwrap();
+        cstr.to_str().unwrap()
+    }
 
     extern "C" fn empty_err_cb(_data: *const u8, _len: c_int) {}
 
     #[test]
     fn agent_create_with_anonymous_should_work() {
-        let url = b"https://ic0.app\0";
         let mut fptr = apply_fptr::<AnonymousIdentity, _>();
         identity_anonymous(&mut fptr);
-        let mut ptr = apply_ptr();
+        let mut ptr = apply_ptr::<AgentWrapper>();
 
         assert_eq!(
             agent_create(
-                url.as_ptr() as *const c_char,
+                IC_NET_BYTES.as_ptr() as *const c_char,
                 &mut fptr,
                 IdentityType::Anonymous,
+                CANISTER_ID_BYTES.as_ptr(),
+                CANISTER_ID_BYTES.len() as c_int,
+                DID_CONTENT_BYTES.as_ptr() as *const c_char,
                 &mut ptr,
                 empty_err_cb
             ),
@@ -104,8 +149,14 @@ mod tests {
             let identity_boxed = Box::from_raw(fptr as *mut dyn Identity);
             assert!(identity_boxed.sender().is_ok());
 
-            let agent_boxed = Box::from_raw(ptr as *mut Agent);
-            assert_eq!(agent_boxed.get_principal(), Ok(Principal::anonymous()));
+            let agent_w_wrap = Box::from_raw(ptr as *mut AgentWrapper);
+            assert_eq!(agent_w_wrap.url, cbytes_to_str(IC_NET_BYTES));
+            assert_eq!(agent_w_wrap.identity.sender(), identity_boxed.sender());
+            assert_eq!(
+                agent_w_wrap.canister_id,
+                Principal::from_slice(CANISTER_ID_BYTES)
+            );
+            assert_eq!(agent_w_wrap.did_content, cbytes_to_str(DID_CONTENT_BYTES));
         }
     }
 
@@ -113,14 +164,16 @@ mod tests {
     fn agent_create_with_secp256k1_should_work() {
         let mut fptr = apply_fptr::<Secp256k1Identity, _>();
         identity_secp256k1_random(&mut fptr);
-
-        let mut ptr = apply_ptr::<Agent>();
+        let mut ptr = apply_ptr::<AgentWrapper>();
 
         assert_eq!(
             agent_create(
-                MAIN_NET.as_ptr() as *const c_char,
+                IC_NET_BYTES.as_ptr() as *const c_char,
                 &mut fptr,
                 IdentityType::Secp256K1,
+                CANISTER_ID_BYTES.as_ptr(),
+                CANISTER_ID_BYTES.len() as c_int,
+                DID_CONTENT_BYTES.as_ptr() as *const c_char,
                 &mut ptr,
                 empty_err_cb
             ),
@@ -130,31 +183,38 @@ mod tests {
         unsafe {
             let identity_boxed = Box::from_raw(fptr as *mut dyn Identity);
             assert!(identity_boxed.sender().is_ok());
-            //
-            let agent_boxed = Box::from_raw(ptr as *mut Agent);
-            assert_eq!(agent_boxed.get_principal(), identity_boxed.sender());
+
+            let agent_w_wrap = Box::from_raw(ptr as *mut AgentWrapper);
+            assert_eq!(agent_w_wrap.url, cbytes_to_str(IC_NET_BYTES));
+            assert_eq!(agent_w_wrap.identity.sender(), identity_boxed.sender());
+            assert_eq!(
+                agent_w_wrap.canister_id,
+                Principal::from_slice(CANISTER_ID_BYTES)
+            );
+            assert_eq!(agent_w_wrap.did_content, cbytes_to_str(DID_CONTENT_BYTES));
         }
     }
 
     #[test]
     fn agent_create_with_basic_should_fail() {
         extern "C" fn err_cb(data: *const u8, _len: c_int) {
-            let c_str = unsafe {CStr::from_ptr(data as *const i8)};
+            let c_str = unsafe { CStr::from_ptr(data as *const i8) };
             let str = c_str.to_str().unwrap();
             assert_eq!(str, "Forbid using [`BasicIdentity`]");
         }
 
-
         let mut fptr = apply_fptr::<BasicIdentity, _>();
-        identity_secp256k1_random(&mut fptr);
-
-        let mut ptr = apply_ptr::<Agent>();
+        identity_basic_random(&mut fptr, empty_err_cb);
+        let mut ptr = apply_ptr::<AgentWrapper>();
 
         assert_eq!(
             agent_create(
-                MAIN_NET.as_ptr() as *const c_char,
+                IC_NET_BYTES.as_ptr() as *const c_char,
                 &mut fptr,
                 IdentityType::Basic,
+                CANISTER_ID_BYTES.as_ptr(),
+                CANISTER_ID_BYTES.len() as c_int,
+                DID_CONTENT_BYTES.as_ptr() as *const c_char,
                 &mut ptr,
                 err_cb
             ),
@@ -165,7 +225,7 @@ mod tests {
             let identity_boxed = Box::from_raw(fptr as *mut dyn Identity);
             assert!(identity_boxed.sender().is_ok());
 
-            assert_eq!(ptr as u64, 0);
+            assert_eq!(ptr as u64, 0u64);
         }
     }
 }
